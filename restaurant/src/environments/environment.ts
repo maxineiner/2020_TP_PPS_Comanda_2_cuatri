c45ad9ce8efa49e366d6115b0c249bda9e5ec0f8
federicotomadin <federico@tangol.com>
2020-10-19 11:59:45 -0300

    configuracion firebase

--- restaurant/src/environments/environment.ts
+++ restaurant/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:  {
+    apiKey: "AIzaSyBhf33JJACQE12cV66MWZmlqVZiFA71-Cw",
+    authDomain: "restaurante-42e1e.firebaseapp.com",
+    databaseURL: "https://restaurante-42e1e.firebaseio.com",
+    projectId: "restaurante-42e1e",
+    storageBucket: "restaurante-42e1e.appspot.com",
+    messagingSenderId: "56067778525",
+    appId: "1:56067778525:web:77d931099e8450ff9d7834",
+    measurementId: "G-ESR12SL90J"
+  }
 };
 
 /*
