a39d5533020b56380422afab922b06d1a3856222
413831 <alepleins3pp@gmail.com>
2020-10-26 11:33:55 -0300

    Servicio QR integrado a DB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,46 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { ComponentsModule } from './components/components.module';
+import { environment } from 'src/environments/environment';
+
+// Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
-import { environment } from 'src/environments/environment';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-import { MediaCapture } from '@ionic-native/media-capture/ngx';
 
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+// Plugins
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { MediaCapture } from '@ionic-native/media-capture/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: 
-  [
-    BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AngularFireAuthModule,
-    AngularFireDatabaseModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    ComponentsModule,
-    AngularFireStorageModule
-  ],
+  imports:
+    [
+      BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      FormsModule,
+      ReactiveFormsModule,
+      AngularFireAuthModule,
+      AngularFireDatabaseModule,
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+      ComponentsModule,
+      AngularFireStorageModule
+    ],
   providers: [
     StatusBar,
     SplashScreen,
-    QRScanner,
+    BarcodeScanner,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     AngularFireDatabase,
     MediaCapture
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { CodigoQRService } from 'src/app/services/codigo-qr.service';
 import { MenuMesaPage } from '../menu-mesa/menu-mesa.page';
 
@@ -8,15 +8,25 @@ import { MenuMesaPage } from '../menu-mesa/menu-mesa.page';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-
-  constructor(private escanerQR: CodigoQRService) {}
+export class Tab1Page
+{
+  constructor(private escanerQR: CodigoQRService,
+    private toastController: ToastController) { }
 
   escanear()
   {
-    let scan = this.escanerQR.escanear(); 
+    let scan = this.escanerQR.escanear("Escanee el DNI", 'QR_CODE');
 
-    console.log(scan);
+    this.presentToast(scan);
+  }
+
+  async presentToast(message)
+  {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 
