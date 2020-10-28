30744a660e72a29e472c8fbedd988e01b0b2a1e1
LeandroEgea <leonicoegea@gmail.com>
2020-10-28 18:54:59 -0300

    componentes de login y registro

---
+++ src/app/components/register/register.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { ClienteLogin } from '../../clases/cliente';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit
+{
+  clienteLogin: ClienteLogin = new ClienteLogin
+
+  // constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService) { }
+
+  ngOnInit()
+  {
+  }
+
+  async onRegister()
+  {
+    const user = await this.authService.onRegister(this.clienteLogin);
+
+    if (user)
+    {
+      console.log('User created!!!');
+      // this.router.navigateByUrl('/');
+    }
+  }
+
+}
