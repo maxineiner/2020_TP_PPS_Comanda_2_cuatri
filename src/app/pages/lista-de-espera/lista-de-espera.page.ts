a3561462f2e99e1f3b91936f7a6596561685d35d
Fede <dfedericovera@gmail.com>
2020-11-03 16:35:18 -0300

    Agrego Listado y cambio de estado

--- src/app/pages/lista-de-espera/lista-de-espera.page.ts
+++ src/app/pages/lista-de-espera/lista-de-espera.page.ts
@@ -2,6 +2,7 @@ import { Component, DoCheck, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 import { Cliente } from 'src/app/clases/cliente';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { UIVisualService } from 'src/app/services/uivisual.service';
 
 @Component({
   selector: 'app-lista-de-espera',
@@ -11,21 +12,23 @@ import { ClienteService } from 'src/app/services/cliente.service';
 export class ListaDeEsperaPage implements OnInit , DoCheck
 {
 
-  clientes: Cliente[] = [];
-  loadingTime = 2000;
+  clientes: Cliente[];
 
   constructor(
     private clienteService: ClienteService,
+    private UIVisual: UIVisualService
   )
   {
     this.clienteService.leer().then(clientes=>{
       this.clientes = clientes.filter(cliente => cliente.enListaDeEspera == true);
+      console.log(this.clientes);
     })
+    
   }
 
   ngDoCheck(): void
   {
-    this.clientes = ClienteService.clientes;
+    this.clientes = ClienteService.clientes.filter(cliente => cliente.enListaDeEspera == true);
   }
 
 
@@ -34,9 +37,15 @@ export class ListaDeEsperaPage implements OnInit , DoCheck
 
   }
 
-  seleccionarClienteDelListado(cliente: Cliente)
-  {
+  quitarDeLaLista(cliente:Cliente){
+    cliente.enListaDeEspera = false;
+    this.clienteService.actualizar(cliente).then(ok=>{
+      UIVisualService.presentToast('Removido correctamente.');
+    });
+  }
 
+  asignarMesa(cliente){
+    
   }
 
 }
