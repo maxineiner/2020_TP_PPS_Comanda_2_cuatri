6eff07a06244aca63993ab51de5ad998215c3cd2
LeandroEgea <leonicoegea@gmail.com>
2020-11-08 18:14:37 -0300

    create page clientes pendientes

---
+++ src/app/pages/clientes-pendientes/clientes-pendientes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes-pendientes',
+  templateUrl: './clientes-pendientes.page.html',
+  styleUrls: ['./clientes-pendientes.page.scss'],
+})
+export class ClientesPendientesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
