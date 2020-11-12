efec7e1c7ef62c97040b59dc3a1466710ae1da7d
Fede <dfedericovera@gmail.com>
2020-11-11 21:42:49 -0300

    Buenas practicas lista de espera

--- src/app/pages/lista-de-espera/lista-de-espera.page.ts
+++ src/app/pages/lista-de-espera/lista-de-espera.page.ts
@@ -22,27 +22,16 @@ export class ListaDeEsperaPage implements OnInit, DoCheck
   clientes: Cliente[];
   mesas: Mesa[];
   clienteActual: Cliente;
-  usuario:Usuario;
+  usuario: Usuario;
 
   constructor(
     private clienteService: ClienteService,
     private UIVisual: UIVisualService,
     private pedidosService: PedidoService,
     private mesasService: MesaService,
-    private rolService:RolesService
+    private rolService: RolesService
   )
-  {
-    this.usuario = AuthService.usuario;
-    this.clienteService.leer().then(clientes =>
-    {
-      this.clientes = clientes.filter(cliente => cliente.enListaDeEspera.isWaiting == true).sort(this.compararFecha);
-      console.log(this.clientes);
-    })
-    this.mesasService.leer().then(mesas =>
-    {
-      this.mesas = mesas.filter(mesa => mesa.isAvailable == true);
-    })
-  }
+  { }
 
   ngDoCheck(): void
   {
@@ -65,7 +54,16 @@ export class ListaDeEsperaPage implements OnInit, DoCheck
 
   ngOnInit() 
   {
-
+    this.usuario = AuthService.usuario;
+    this.clienteService.leer().then(clientes =>
+    {
+      this.clientes = clientes.filter(cliente => cliente.enListaDeEspera.isWaiting == true).sort(this.compararFecha);
+      console.log(this.clientes);
+    })
+    this.mesasService.leer().then(mesas =>
+    {
+      this.mesas = mesas.filter(mesa => mesa.isAvailable == true);
+    })
   }
 
   quitarDeLaLista(cliente: Cliente)
