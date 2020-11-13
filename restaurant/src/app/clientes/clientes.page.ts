84c710c826bbd2c309856606ba3e0a82c5d82280
carbone2001 <lucascarbone2000@hotmail.com>
2020-11-13 12:09:40 -0300

    Vuelvo a activar codigo de pedidos

--- restaurant/src/app/clientes/clientes.page.ts
+++ restaurant/src/app/clientes/clientes.page.ts
@@ -70,22 +70,22 @@ export class ClientesPage implements OnInit {
   }
 
   escanearQR(): void {
-     this.irPedidoActivo();
+     //this.irPedidoActivo();
      
-    //  this.barcodeScanner.scan({ formats: 'QR_CODE' }).then((data) => {
-    //    if (data && !data.cancelled) {
-    //      if (data.text === 'LISTA_DE_ESPERA') { // Si usa el QR de lista de espera lo llevamos a LE
-    //        this.irListaEspera();
-    //      }
-    //      else if (this.pedidosActivos.length > 0 && data.text === this.pedidosActivos[0].mesa.id) {
-    //       // Si tiene pedidos activos y coincide con el codigo de qr de la mesa asignada lo llevamo al pedido
-    //        this.irPedidoActivo();
-    //      } else {
-    //        // No existe QR o no es de la mesa asignada
-    //        this.vibrationService.vibrar(500);
-    //        this.utilsService.presentAlert('Lo sentimos', '', 'El código escaneado no existe o no es de la mesa asignada');
-    //      }
-    //    }
-    //  }, (err) => this.utilsService.handleError(err));
+     this.barcodeScanner.scan({ formats: 'QR_CODE' }).then((data) => {
+       if (data && !data.cancelled) {
+         if (data.text === 'LISTA_DE_ESPERA') { // Si usa el QR de lista de espera lo llevamos a LE
+           this.irListaEspera();
+         }
+         else if (this.pedidosActivos.length > 0 && data.text === this.pedidosActivos[0].mesa.id) {
+          // Si tiene pedidos activos y coincide con el codigo de qr de la mesa asignada lo llevamo al pedido
+           this.irPedidoActivo();
+         } else {
+           // No existe QR o no es de la mesa asignada
+           this.vibrationService.vibrar(500);
+           this.utilsService.presentAlert('Lo sentimos', '', 'El código escaneado no existe o no es de la mesa asignada');
+         }
+       }
+     }, (err) => this.utilsService.handleError(err));
   }
 }
