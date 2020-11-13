dfbeae384bf7f215bc06e6f3b45a5f78681610b1
TomasGiordano <tomiigiordano@gmail.com>
2020-11-13 15:08:58 -0300

    Agrego configuracion firebas

--- PPS-LaComanda/src/environments/environment.prod.ts
+++ PPS-LaComanda/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBD_7qk12-D3oEuEr668JcmagWZVDhRVwA",
+    authDomain: "comanda-pgr.firebaseapp.com",
+    databaseURL: "https://comanda-pgr.firebaseio.com",
+    projectId: "comanda-pgr",
+    storageBucket: "comanda-pgr.appspot.com",
+    messagingSenderId: "747777786917",
+    appId: "1:747777786917:web:7f9adb1a8e8ff6dcee6bcc",
+    measurementId: "G-X1EC9BCZ80"
+  }
 };
