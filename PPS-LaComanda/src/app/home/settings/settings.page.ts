f18c2711a901ce603c5a7d47af2197d1592c11fd
TomasGiordano <tomas.giordano@boreal-it.com>
2021-02-06 22:50:47 -0300

    ultimos cambios

--- PPS-LaComanda/src/app/home/settings/settings.page.ts
+++ PPS-LaComanda/src/app/home/settings/settings.page.ts
@@ -8,16 +8,50 @@ import { LenguajesService } from '../../servicios/lenguajes.service';
 })
 export class SettingsPage implements OnInit {
 
-  langList : any[] = [];
+  lang : string = '';
+  langList : any[]= [
+    {text: 'lenguajes.paises.AR', value: 'ar'},
+      {text: 'lenguajes.paises.CH', value: 'ch'},
+      {text: 'lenguajes.paises.PE', value: 'pe'},
+      {text: 'lenguajes.paises.PA', value: 'pa'},
+      {text: 'lenguajes.paises.ME', value: 'mx'},
+      {text: 'lenguajes.paises.CR', value: 'cr'},
+      {text: 'lenguajes.paises.UR', value: 'uy'},
+      {text: 'lenguajes.paises.ES', value: 'es'},
+
+      {text: 'lenguajes.paises.EU', value: 'us'},
+      {text: 'lenguajes.paises.CA', value: 'ca'},
+      {text: 'lenguajes.paises.AU', value: 'au'},
+      {text: 'lenguajes.paises.NZ', value: 'nz'},
+      {text: 'lenguajes.paises.FI', value: 'ph'},
+
+      {text: 'lenguajes.paises.FR', value: 'fr'},
+      {text: 'lenguajes.paises.BE', value: 'bj'},
+      {text: 'lenguajes.paises.BF', value: 'bf'},
+
+      {text: 'lenguajes.paises.UC', value: 'ua'},
+      {text: 'lenguajes.paises.RU', value: 'ru'},
+
+      {text: 'lenguajes.paises.AL', value: 'de'},
+
+      {text: 'lenguajes.paises.BR', value: 'br'},
+      {text: 'lenguajes.paises.PO', value: 'pt'},
+  ];
+  
 
   constructor(private languageService: LenguajesService) { }
 
   ngOnInit() {
-    this.langList = this.languageService.getLang();
+    this.languageService.getFlag().then(val => {
+      this.lang = val
+    });
   }
   changeLang(event){
     console.log(event);
-    this.languageService.setLang(event);
+    this.languageService.setFlag(event.value);
+    this.languageService.selectLang(event.text);
+    this.lang = event.value;
   }
+
 
 }
--- PPS-LaComanda/src/app/servicios/lenguajes.service.ts
+++ PPS-LaComanda/src/app/servicios/lenguajes.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { from } from 'rxjs';
 import { Storage } from '@ionic/storage';
-const LNG_KEY = 'SELECTED_LANGUAGE';
 
+const LNG_KEY = 'SELECTED_LANGUAGE';
+const FLAG_KEY = 'SELECTED_FLAG';
 
 @Injectable({
   providedIn: 'root'
@@ -27,16 +28,36 @@ export class LenguajesService {
       this.setLang(lang);
       this.selected = lang; 
     });
+    this.storage.get(FLAG_KEY).then(val => {
+      if(!val){
+        this.setFlag('ar');
+      }
+    })
   }
 
   getLang(){
     return [
-      { text: 'lenguajes.opciones.INGLES', value: 'en'},
-      { text: 'lenguajes.opciones.ESPAÑOL', value: 'es'},
-      { text: 'lenguajes.opciones.FRANCES', value: 'fr'},
-      { text: 'lenguajes.opciones.RUSO', value: 'ru'},
-      { text: 'lenguajes.opciones.ALEMAN', value: 'al'},
-      { text: 'lenguajes.opciones.PORTUGUES', value: 'po'},
+      {text: 'lenguajes.paises.AR', value: 'es'},
+      {text: 'lenguajes.paises.CH', value: 'es'},
+      {text: 'lenguajes.paises.PE', value: 'es'},
+      {text: 'lenguajes.paises.PA', value: 'es'},
+      {text: 'lenguajes.paises.ME', value: 'es'},
+      {text: 'lenguajes.paises.CR', value: 'es'},
+      {text: 'lenguajes.paises.UR', value: 'es'},
+      {text: 'lenguajes.paises.ES', value: 'es'},
+      {text: 'lenguajes.paises.EU', value: 'en'},
+      {text: 'lenguajes.paises.CA', value: 'en'},
+      {text: 'lenguajes.paises.AU', value: 'en'},
+      {text: 'lenguajes.paises.NZ', value: 'en'},
+      {text: 'lenguajes.paises.FI', value: 'en'},
+      {text: 'lenguajes.paises.FR', value: 'fr'},
+      {text: 'lenguajes.paises.BE', value: 'fr'},
+      {text: 'lenguajes.paises.BF', value: 'fr'},
+      {text: 'lenguajes.paises.UC', value: 'ru'},
+      {text: 'lenguajes.paises.RU', value: 'ru'},
+      {text: 'lenguajes.paises.AL', value: 'al'},
+      {text: 'lenguajes.paises.BR', value: 'po'},
+      {text: 'lenguajes.paises.PO', value: 'po'},
     ];
   }
 
@@ -46,4 +67,16 @@ export class LenguajesService {
     this.storage.set(LNG_KEY,lang)
   }
 
+  selectLang(lang){
+    let aux : any = this.getLang().filter((a)=> a.text === lang);
+    this.setLang(aux[0].value);
+  }
+
+  setFlag(flag){
+    return this.storage.set(FLAG_KEY,flag);
+  }
+
+  getFlag(){
+    return this.storage.get(FLAG_KEY);
+  }
 }
